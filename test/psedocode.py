4e20f1b49f3d210ace0b3f45c3878e35fa796512
richmondmason-bit <richmond.mason@ucas-edu.net>
2026-03-18 09:53:38 -0600

    mostly worked on pygame text insert

---
+++ test/psedocode.py
@@ -0,0 +1,18 @@
+#import pygame and sys
+#define window height and width and name 
+#screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#font  = pygame.font (font,size)
+#set biggest character width and height(capital A)
+
+#grid size 
+#cols = width// charcter width
+#rows = height // charcter height
+
+#bg_colors = (color code )
+#color_text = (color code)
+
+#class cell or area you text (has to be init since its base)
+#def __init__ (self,char = "",color = color_text, image = none)
+    #selt.char = char 
+    #self.color
